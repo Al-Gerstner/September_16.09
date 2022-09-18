5b976cd6acc59f68695bd7be96ec8d03c1dd5122
Al_Gerstner <waxing-moon@mail.ru>
2022-09-18 12:32:09 +0700

    Random

--- Example/example_004_Dif/Program.cs
+++ Example/example_004_Dif/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿int namberA = new Random().Next(1, 10); // 1 2 3 4 ..... 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
+Console.WriteLine(namberA + namberB);
